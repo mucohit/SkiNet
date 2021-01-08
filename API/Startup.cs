cf0d0b84168c6948b105b83c039afddefecd90ea
Mucahit Aric <aricmucahit@gmail.com>
2021-01-07 21:37:12 -0500

    Seed Data, ProductBrand, ProductType

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -24,6 +25,7 @@ namespace API
         {
 
             services.AddControllers();
+            services.AddScoped<IProductRepository, ProductRepository>();
 
             //Connection String
             services.AddDbContext<StoreContext>(x =>
