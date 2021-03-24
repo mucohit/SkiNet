4eae4e1009768670db3a021c2159421c4e2c58a9
Mucahit Aric <aricmucahit@gmail.com>
2021-03-23 20:20:06 -0400

    Routing

---
+++ client/src/app/shop/product-detail/product-detail.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+  product: IProduct;
+
+  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  // tslint:disable-next-line: typedef
+  loadProduct() {
+    // tslint:disable-next-line: deprecation
+    this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(product => {
+      this.product = product;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+}
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -6,6 +6,7 @@ import { IBrand } from '../shared/models/brands';
 import { IType } from '../shared/models/productType';
 import { delay, map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -43,6 +44,11 @@ export class ShopService {
           return response.body;
         })
         );
+  }
+
+  // tslint:disable-next-line: typedef
+  getProduct(id: number){
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   // tslint:disable-next-line: typedef
