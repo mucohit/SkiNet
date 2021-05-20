26cdd3a9a904b3db5a6f050240a27e7d00e041d4
Mucahit Aric <aricmucahit@gmail.com>
2021-05-19 20:30:11 -0400

    basket

--- API/Startup.cs
+++ API/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -37,8 +38,15 @@ namespace API
             //Connection String
             services.AddDbContext<StoreContext>(x =>
              x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddSingleton<IConnectionMultiplexer>(c => {
+                var configuration = ConfigurationOptions.Parse(_configuration.GetConnectionString("Redis"),
+                true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });
            
             services.AddApplicationServices();
+
             
            services.AddSwaggerDocumentation();
 
